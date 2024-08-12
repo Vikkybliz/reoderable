30c267e834632aa38c1996674240b5162fedb81e
Victor Ayomide <vikkybliz01@gmail.com>
2024-08-12 07:17:08 +0100

    youtube

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<int> _items = List.generate(20, (index) => index);
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: ReorderableListView.builder(
+        itemBuilder: (context, index){
+          return Text(
+            key: ValueKey(_items[index]),
+            _items[index].toString());
+        }, 
+        itemCount: _items.length, 
+        onReorder: (start, end) {
+          setState(() {
+            if(start < end){
+              end -=1;
+            }
+            print(start);
+            print(end);
+            final int item = _items.removeAt(start);
+            _items.insert(end, item);
+          });
+        }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
